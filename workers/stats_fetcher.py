25bdff94079247a72886fbb737549a39ae863836
davidg-sdcc <david.gonchar@gmail.com>
2022-11-08 18:19:02 +0200

    FLOW-33: added errors monitoring via API requests

--- workers/stats_fetcher.py
+++ workers/stats_fetcher.py
@@ -20,6 +20,20 @@ class StatsFetcher(BaseWorker):
 
     _seconds_interval = 60
 
+    def report_task_failure(self, *args, **kwargs):
+        try:
+            result = self.wrap_result(success=False, payload=list(args), cur_time=True)
+            params = dict(request_type=REQUEST_TYPE.SEND_STATS, identifier=self._identifier, payload=result)
+            params.update({k: v for k, v in self.args.items() if k not in params})
+
+            result = send_request(config=self.args, **params)
+
+            return result
+        except Exception as e:
+            err_msg = f'failed report_task_failure - "{str(e)}"'
+            Log.exception(err_msg)
+            return None
+
     def work(self,
              credentials: Optional[Dict[str, Any]] = None,
              *args, **kwargs):
@@ -31,15 +45,17 @@ class StatsFetcher(BaseWorker):
 
         command_worker = self.init_command_worker(credentials=credentials)
         if not command_worker:
-            Log.error(f'failed to initialize command_worker - vendor: "{self.vendor}"')
-            return self.report_task_failure()
+            err_msg = f'failed to initialize command_worker - vendor: "{self.vendor}"'
+            Log.error(err_msg)
+            return self.report_task_failure(err_msg)
 
         router_flows = self.get_flows_from_router(command_worker=command_worker,
                                                   credentials=credentials,
                                                   flow_number=True)
         if router_flows is None:
-            Log.warning(f'an error occurred while trying to get flows from the router')
-            return self.report_task_failure()
+            err_msg = f'an error occurred while trying to get flows from the router'
+            Log.warning(err_msg)
+            return self.report_task_failure(err_msg)
 
         try:
             result = self.wrap_result(success=True,
@@ -47,8 +63,9 @@ class StatsFetcher(BaseWorker):
                                       cur_time=True)
         except Exception as ex:
             logged = f'logged - ' if isinstance(ex, LException) else ''
-            Log.exception(f'failed to wrap result to api - {logged}error: "{str(ex)}"')
-            return self.report_task_failure()
+            err_msg = f'failed to wrap result to api - {logged}error: "{str(ex)}"'
+            Log.exception(err_msg)
+            return self.report_task_failure(err_msg)
 
         params = dict(request_type=REQUEST_TYPE.SEND_STATS,
                       identifier=self._identifier,
@@ -59,9 +76,10 @@ class StatsFetcher(BaseWorker):
             result = send_request(config=self.args, **params)
         except Exception as ex:
             logged = f'logged - ' if isinstance(ex, LException) else ''
-            Log.exception(f'failed to send stats to BE api - {logged}error: "{str(ex)}"')
+            err_msg = f'failed to send stats to BE api - {logged}error: "{str(ex)}"'
+            Log.exception(err_msg)
             self.set_failed_api_call()
-            return self.report_task_failure()
+            return self.report_task_failure(err_msg)
         self.set_success_api_call()
 
         end_time = datetime.datetime.utcnow()
