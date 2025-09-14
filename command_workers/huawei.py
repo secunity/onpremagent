1d6362e395859aa2ed7b31a327766b0503e141f4
Meir Elbaz <meire@flow-sec.com>
2025-09-14 15:41:11 +0300

    use no-more

--- command_workers/huawei.py
+++ command_workers/huawei.py
@@ -8,8 +8,8 @@ from common.enums import VENDOR
 class HuaweiCommandWorker(SshCommandWorker):
     _default_vpn_name = 'LAB-VPN'
 
-    _get_stats_from_router_command = 'display bgp flow vpnv4 vpn-instance {} routing-table'
-    _get_stats_for_sig = 'display flowspec vpnv4 vpn-instance {} statistics {}'
+    _get_stats_from_router_command = 'display bgp flow vpnv4 vpn-instance {} routing-table | no-more'
+    _get_stats_for_sig = 'display flowspec vpnv4 vpn-instance {} statistics {} | no-more'
 
     @property
     def vendor(self) -> VENDOR:
@@ -32,30 +32,13 @@ class HuaweiCommandWorker(SshCommandWorker):
 
             for idx in re.findall(r"ReIndex\s*:\s*(\d+)", '\n'.join(result)):
                 try:
-                    stdin, stdout, stderr = connection.exec_command(self._get_stats_for_sig.format(vpn_name, idx))
-                    for _ in range(10):
-                        stdin.write('\n')
-                    stdin.flush()
-                    result_idx = stdout.readlines()
-                    for s in result_idx:
-                        print("line", s)
-                        result.append(re.sub(r"\s*---- More ----\x1b\[16D\s*\x1b\[16D", "", s))
+                    _, stdout, stderr = connection.exec_command(self._get_stats_for_sig.format(vpn_name, idx))
+                    result += stdout.readlines()
                 except Exception as er:
                     print(f"error idx {idx}: {er}")
 
             output = [line.rstrip('\r\n') for line in result]
 
-            print(output)
-
             return output
         finally:
             connection.close()
-
-
-
-
-
-
-
-
-
