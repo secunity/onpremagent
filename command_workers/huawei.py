243f5b33d4dd171018ff543edc5beed98a28984a
shem <shemb@flow-sec.com>
2025-09-14 13:04:44 +0300

    split ipv6 and ipv4

--- command_workers/huawei.py
+++ command_workers/huawei.py
@@ -1,40 +1,40 @@
 import re
 from typing import Optional, Dict, List, Callable
-
 from command_workers.bases import SshCommandWorker
 from common.enums import VENDOR
 
 
 class HuaweiCommandWorker(SshCommandWorker):
-
     _default_vpn_name = 'LAB-VPN'
-
-    _get_stats_from_router_command = 'display bgp flow vpnv4 vpn-instance {} routing-table'
-    _get_stats_for_sig = 'display flowspec vpnv4 vpn-instance {} statistics {}'
+    _get_stats_from_router_command_v4 = 'display bgp flow vpnv4 vpn-instance {} routing-table'
+    _get_stats_from_router_command_v6 = 'display bgp flow vpnv6 vpn-instance {} routing-table'
+    _get_stats_for_sig_v4 = 'display flowspec vpnv4 vpn-instance {} statistics {}'
+    _get_stats_for_sig_v6 = 'display flowspec vpnv6 vpn-instance {} statistics {}'
 
     @property
     def vendor(self) -> VENDOR:
         return VENDOR.HUAWEI
 
-    def execute_cli(self,
-                    credentials: Dict[str, object],
-                    command: str = None,
-                    exec_command: Optional[Callable] = None,
-                    **kwargs) -> List[str]:
+    def execute_cli(self, credentials: Dict[str, object], command: str = None,
+                    exec_command: Optional[Callable] = None, **kwargs) -> List[str]:
         vpn_name = kwargs.get('vrf', self._default_vpn_name)
+        is_ipv6 = kwargs.get('is_ipv6', False)  # Add this parameter to determine IPv4/IPv6
+
+        # Choose the appropriate commands based on IPv4/IPv6
+        if is_ipv6:
+            main_command = self._get_stats_from_router_command_v6
+            stats_command = self._get_stats_for_sig_v6
+        else:
+            main_command = self._get_stats_from_router_command_v4
+            stats_command = self._get_stats_for_sig_v4
 
         connection = self.generate_connection(credentials, **kwargs)
         try:
-            # Build the command string explicitly
-            cmd = self._get_stats_from_router_command.format(vpn_name or self._default_vpn_name)
-            self.logger.debug(f'SSH command: "{cmd}"')  # <- so you see the real one
-            _, stdout, stderr = connection.exec_command(cmd)
+            _, stdout, stderr = connection.exec_command(main_command.format(vpn_name))
             result = stdout.readlines()
 
             for idx in re.findall(r"ReIndex\s*:\s*(\d+)", '\n'.join(result)):
-                sig_cmd = self._get_stats_for_sig.format(vpn_name, idx)
-                self.logger.debug(f'SSH command: "{sig_cmd}"')
-                _, stdout, stderr = connection.exec_command(sig_cmd)
+                _, stdout, stderr = connection.exec_command(stats_command.format(vpn_name, idx))
                 result.extend(stdout.readlines())
 
             return [line.rstrip('\r\n') for line in result]
