eec583f6050a75337edb13cf56e0fcec026be5a7
Meir Elbaz <meire@flow-sec.com>
2025-09-29 13:30:44 +0300

    fix huawei: multi command on single connection

--- command_workers/huawei.py
+++ command_workers/huawei.py
@@ -1,44 +1,59 @@
 import re
-from typing import Optional, Dict, List, Callable
 
 from command_workers.bases import SshCommandWorker
 from common.enums import VENDOR
+from common.logs import Log
+from common.sshutils import read_and_wait
 
 
 class HuaweiCommandWorker(SshCommandWorker):
-    _default_vpn_name = 'LAB-VPN'
+    _get_stats_from_router_command = "display bgp"
 
-    _get_stats_from_router_command = 'display bgp flow vpnv4 vpn-instance {} routing-table | no-more'
-    _get_stats_for_sig = 'display flowspec vpnv4 vpn-instance {} statistics {} | no-more'
+    SHELL_PROMPT = re.compile(r"<.*?>")
+
+    DISPLAY_ROUTING_TABLE = "display bgp flow vpnv4 vpn-instance {vpn_instance} routing-table | no-more"
+    DISPLAY_STATISTICS = "display flowspec vpnv4 vpn-instance {vpn_instance} statistics {re_index} | no-more"
 
     @property
     def vendor(self) -> VENDOR:
         return VENDOR.HUAWEI
 
-    def execute_cli(self,
-                    credentials: Dict[str, object],
-                    command: str = None,
-                    exec_command: Optional[Callable] = None,
-                    **kwargs) -> List[str]:
-        vpn_name = kwargs.get('vrf', self._default_vpn_name)
+    def execute_cli(self, credentials, command = None, exec_command = None, **kwargs):
+        stats_type = kwargs.get("stats_type")
+        if stats_type == "IPv6":
+            Log.info("No IPv6 support")
+            return []
 
-        connection = self.generate_connection(credentials, **kwargs)
         try:
-            _, stdout, stderr = connection.exec_command(self._get_stats_from_router_command.format(vpn_name))
-            result = stdout.readlines()
+            vpn_instance = kwargs.get("vrf")
 
-            for idx in re.findall(r"ReIndex\s*:\s*(\d+)", '\n'.join(result)):
-                try:
-                    connection.close()
-                    connection = self.generate_connection(credentials, **kwargs)
+            ssh_client = self.generate_connection(credentials, **kwargs)
 
-                    _, stdout, stderr = connection.exec_command(self._get_stats_for_sig.format(vpn_name, idx))
-                    result += stdout.readlines()
-                except Exception as er:
-                    print(f"error idx {idx}: {er}")
+            shell = ssh_client.invoke_shell()
 
-            output = [line.rstrip('\r\n') for line in result]
+            output_array = []
 
-            return output
-        finally:
-            connection.close()
+            output = read_and_wait(shell, self.SHELL_PROMPT)
+
+            command = f"{self.DISPLAY_ROUTING_TABLE.format(vpn_instance=vpn_instance)}\n"
+            Log.debug(f"Executing command: {command.strip()}")
+            shell.sendall(command)
+            output = read_and_wait(shell, self.SHELL_PROMPT)
+
+            output_array += output.splitlines()
+
+            for re_index in re.findall(r"ReIndex\s*:\s*(\d+)", output):
+                Log.debug(f"Get statistics for: {re_index}")
+                command = f"{self.DISPLAY_STATISTICS.format(vpn_instance=vpn_instance, re_index=re_index)}\n"
+                shell.sendall(command)
+                output = read_and_wait(shell, self.SHELL_PROMPT)
+
+                output_array += ["\f"]
+                output_array += output.splitlines()
+
+            ssh_client.close()
+
+            return output_array
+        except Exception as e:
+            Log.error(f"Error executing CLI command: {str(e)}")
+            return []
